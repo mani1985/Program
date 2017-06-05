82731b6e90e20b87e0e2e8efde27c3da19b189bc
mani1985 <hmanimca@gmail.com>
2017-06-05 14:20:54 +0530

    image

---
+++ label_image.py
@@ -0,0 +1,56 @@
+import os, sys
+
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# change this as you see fit
+image_path = sys.argv[1]
+
+# Read in the image_data
+image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+# Loads label file, strips off carriage return
+label_lines = [line.rstrip() for line 
+                   in tf.gfile.GFile("retrained_labels.txt")]
+
+# Unpersists graph from file
+with tf.gfile.FastGFile("retrained_graph.pb", 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    tf.import_graph_def(graph_def, name='')
+
+with tf.Session() as sess:
+    # Feed the image_data as input to the graph and get first prediction
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    
+    predictions = sess.run(softmax_tensor, \
+             {'DecodeJpeg/contents:0': image_data})
+    
+    # Sort to show labels of first prediction in order of confidence
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    #print("exact match only")
+    #print("top_k",top_k)
+    #human_string_matched = label_lines[0]
+    #score_matched = predictions[0][0]
+    #print('matched  %s (score = %.5f)' % (human_string_matched, score_matched))
+    #for node_id in top_k:
+        #human_string = label_lines[node_id]
+        #score = predictions[0][node_id]
+        #print("node_id",node_id)
+        #print('%s (score = %.5f)' % (human_string, score))
+
+    
+    Flag = False
+
+    for node_id in top_k:
+        human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        if(score > 0.5):
+           Flag = True
+           print('Food item matched:  %s (score = %.5f)' % (human_string, score))
+             
+
+    if(Flag == False):
+        print('No food item matched for the input image')     
+
